e097512429e99ea2395fa7b5b2418067702ec7cc
SonCT <32060631+vndkubi@users.noreply.github.com>
2018-08-12 16:04:34 +0700

    [Son] config UserVO

--- src/main/java/jvb002/bookstore/app/dto/UserVO.java
+++ src/main/java/jvb002/bookstore/app/dto/UserVO.java
@@ -2,8 +2,66 @@ package jvb002.bookstore.app.dto;
 
 public class UserVO {
 
-	public UserVO() {
-		// TODO Auto-generated constructor stub
+	private int id;
+	private String userName;
+	private String password;
+	private int role;
+	private String fullName;
+	private int createdUserId;
+	private String createdDate;
+	
+	public UserVO(int id) {
+		this.id = id;
 	}
-
+	
+	public UserVO(int id, String userName, String password, int role) {
+		this.id = id;
+		this.userName = userName;
+		this.password = password;
+		this.role = role;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getRole() {
+		return role;
+	}
+	public void setRole(int role) {
+		this.role = role;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public int getCreatedUserId() {
+		return createdUserId;
+	}
+	public void setCreatedUserId(int createdUserId) {
+		this.createdUserId = createdUserId;
+	}
+	public String getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(String createdDate) {
+		this.createdDate = createdDate;
+	}
+	
 }
