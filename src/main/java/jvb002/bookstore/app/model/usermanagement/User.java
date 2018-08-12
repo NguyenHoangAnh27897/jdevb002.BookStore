2fa0eb59caaa8f194a33eb3d2b68af94166640b5
SonCT <32060631+vndkubi@users.noreply.github.com>
2018-08-12 15:55:34 +0700

    [Son] create entity user.

--- src/main/java/jvb002/bookstore/app/model/usermanagement/User.java
+++ src/main/java/jvb002/bookstore/app/model/usermanagement/User.java
@@ -1,5 +1,88 @@
 package jvb002.bookstore.app.model.usermanagement;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
 public class User {
 
+	@Id
+	@Column(name = "UserID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column(name = "UserName")
+	private String userName;
+	@Column(name = "Password")
+	private String password;
+	@Column(name = "Role")
+	private int role;
+	@Column(name = "Fullname")
+	private String fullName;
+	@Column(name = "Created_UserID")
+	private int createdUserId;
+	@Column(name = "Created_Date")
+	private String createdDate;
+	
+	public User(int id) {
+		this.id = id;
+	}
+	
+	public User(int id, String userName, String password, int role) {
+		this.id = id;
+		this.userName = userName;
+		this.password = password;
+		this.role = role;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getRole() {
+		return role;
+	}
+	public void setRole(int role) {
+		this.role = role;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public int getCreatedUserId() {
+		return createdUserId;
+	}
+	public void setCreatedUserId(int createdUserId) {
+		this.createdUserId = createdUserId;
+	}
+	public String getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(String createdDate) {
+		this.createdDate = createdDate;
+	}
+	
+	
 }
