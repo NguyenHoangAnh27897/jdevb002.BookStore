af70c81fbdc7b22d1328fbcf902dcb819ae873c9
Admin <Admin@ASUS>
2018-08-16 13:19:20 +0700

    [hanh] categoryVO

--- src/main/java/jvb002/bookstore/app/dto/CategoryVO.java
+++ src/main/java/jvb002/bookstore/app/dto/CategoryVO.java
@@ -1,9 +1,57 @@
 package jvb002.bookstore.app.dto;
 
+import java.util.Date;
+
 public class CategoryVO {
 
-	public CategoryVO() {
-		// TODO Auto-generated constructor stub
+	private int categoryID;
+
+	private String description;
+
+	private int category_parentID;
+
+	private int created_userID;
+
+	private Date created_date;
+
+	public int getCategoryID() {
+		return categoryID;
+	}
+
+	public void setCategoryID(int categoryID) {
+		this.categoryID = categoryID;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getCategory_parentID() {
+		return category_parentID;
+	}
+
+	public void setCategory_parentID(int category_parentID) {
+		this.category_parentID = category_parentID;
+	}
+
+	public int getCreated_userID() {
+		return created_userID;
+	}
+
+	public void setCreated_userID(int created_userID) {
+		this.created_userID = created_userID;
+	}
+
+	public Date getCreated_date() {
+		return created_date;
+	}
+
+	public void setCreated_date(Date created_date) {
+		this.created_date = created_date;
 	}
 
 }
