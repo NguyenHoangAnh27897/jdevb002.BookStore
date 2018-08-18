9536884c5b8780ed9a89a3796e808e468af3ed9f
GIT <GIT@ADMIN>
2018-08-18 13:47:49 +0700

    [Nam] add Dao of Bookmanagemnent

---
+++ src/main/java/jvb002/bookstore/app/dao/bookmanagement/BookDAO.java
@@ -0,0 +1,13 @@
+package jvb002.bookstore.app.dao.bookmanagement;
+import java.util.List;
+
+import jvb002.bookstore.app.model.bookmanagement.Book;
+import jvb002.bookstore.app.model.classification.Category;
+import jvb002.bookstore.app.model.publishermanagement.Publisher;
+
+public interface BookDAO {
+	public void create(Book book);
+	public void update(Book book);
+	public List<Category> getCategory();
+	public List<Publisher> getPublisher();
+}
---
+++ src/main/java/jvb002/bookstore/app/dao/bookmanagement/BookDAOImpl.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package jvb002.bookstore.app.dao.bookmanagement;
+
+import java.util.List;
+
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+import jvb002.bookstore.app.model.bookmanagement.Book;
+import jvb002.bookstore.app.model.classification.Category;
+import jvb002.bookstore.app.model.publishermanagement.Publisher;
+
+/**
+ * @author GIT
+ *
+ */
+public class BookDAOImpl extends HibernateDaoSupport implements BookDAO {
+
+	/* (non-Javadoc)
+	 * @see jvb002.bookstore.app.dao.bookmanagement.BookDAO#create(jvb002.bookstore.app.model.bookmanagement.Book)
+	 */
+	@Override
+	public void create(Book book) {
+		// TODO Auto-generated method stub
+		getHibernateTemplate().save(book);
+	}
+
+	/* (non-Javadoc)
+	 * @see jvb002.bookstore.app.dao.bookmanagement.BookDAO#update(jvb002.bookstore.app.model.bookmanagement.Book)
+	 */
+	@Override
+	public void update(Book book) {
+		// TODO Auto-generated method stub
+		getHibernateTemplate().update(book);
+	}
+
+	/* (non-Javadoc)
+	 * @see jvb002.bookstore.app.dao.bookmanagement.BookDAO#getCategory()
+	 */
+	@Override
+	public List<Category> getCategory() {
+		// TODO Auto-generated method stub
+		List<Category> cateList =  (List<Category>) getHibernateTemplate().find("from Category");
+		return cateList.size() > 0 ? cateList : null;
+	}
+
+	/* (non-Javadoc)
+	 * @see jvb002.bookstore.app.dao.bookmanagement.BookDAO#getPublisher()
+	 */
+	@Override
+	public List<Publisher> getPublisher() {
+		// TODO Auto-generated method stub
+		List<Publisher> publisherList =  (List<Publisher>) getHibernateTemplate().find("from Publisher");
+		return publisherList.size() > 0 ? publisherList : null;
+	}
+
+}
