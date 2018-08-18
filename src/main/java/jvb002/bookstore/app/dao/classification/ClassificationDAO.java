5363e62dc4b5bd02352ba7f931562212863b2257
GIT <GIT@ADMIN>
2018-08-18 13:40:36 +0700

    change name

--- src/main/java/jvb002/bookstore/app/dao/classification/ClassificationDAO.java
+++ src/main/java/jvb002/bookstore/app/dao/classification/ClassificationDAO.java
@@ -1,15 +1,16 @@
 package jvb002.bookstore.app.dao.classification;
 
+
+
 import java.util.List;
 
 import jvb002.bookstore.app.model.classification.Category;
 
-public interface ClassificationDAO {
+public interface classificationDAO {
 	public void save(Category c);
 	public void update(Category c);
 	public void delete(int id);
-	public Category getOne(int id,Boolean parentID);
+	public Category getOne(int id);
 	public List<Category> getAll();
-	
 	
 }
